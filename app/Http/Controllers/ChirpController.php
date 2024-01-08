4d926250a015b384baf42a568e37f0e31c1b3fd0
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2024-01-08 11:07:20 -0600

    Showing chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -12,7 +12,7 @@ class ChirpController extends Controller
      */
     public function index():View
     {
-        return view('chirps.index');
+        return view('chirps.index',['chirps'=>Chirp::with('user')->latest()->get()]);
     }
 
     /**
@@ -66,4 +66,5 @@ class ChirpController extends Controller
     {
         //
     }
+
 }
